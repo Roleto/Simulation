3d999f678b398d9d85c8ee7cd2e518d868b4b931
Roland Gulya <mgulyaroland@gmail.com>
2025-11-21 15:54:46 +0100

    finding max value and testing it

--- src/my_duffing.jl
+++ src/my_duffing.jl
@@ -18,7 +18,7 @@ SingleRun = 1
 #########
 δt = 1e-3
 N = Int(2e4)
-last_idx = N - 1
+LONG = N - 1
 
 ######################
 # Control Parameters #
@@ -91,12 +91,12 @@ function log()
 	close(file)
 end
 
-function singlerun(h_int, q, q_p, q_pp, qN_p, qN_pp, u, idx, lastPlot = true)
-	print('.')
-	if (Ploting == 1 && SingleRun == 0 && lastPlot)
-		close("all")
-	end
-	for t ∈ 1:l
+function singlerun(q, q_p, ploting = true)
+	q_mem[1] = q
+	q_p_mem[1] = q_p
+	max_index = 1
+	h_int = 0
+	for t ∈ 1:(LONG-1)
 		time_mem[t] = t * δt
 
 		#define the nominal trajectory
@@ -105,6 +105,12 @@ function singlerun(h_int, q, q_p, q_pp, qN_p, qN_pp, u, idx, lastPlot = true)
 		# Compute the Errors
 		h = qN_mem[t] - q_mem[t]
 		h_p = qN_p_mem[t] - q_p_mem[t]
+
+		# Finding max value
+		h_max = qN_mem[max_index] - q_mem[max_index]
+		if (h*h) < (h_max*h_max)
+			max_index = t
+		end
 
 		if Robust == 1
 			S = ErrorMetrics(h_int, h, h_p)
@@ -130,62 +136,63 @@ function singlerun(h_int, q, q_p, q_pp, qN_p, qN_pp, u, idx, lastPlot = true)
 		q_mem[t+1] = q_mem[t] + δt * q_p_mem[t]
 		h_int = h_int + δt * h
 	end
+	if ploting
+		# Plotting 
+		figure("Trajectory_tracking")
+		grid(true)
+		title("Pályakövetés az idő függvényében")
+		xlabel("Idő [s]")
+		ylabel("Pozíció [m]")
+		plot(time_mem[1:l], qN_mem[1:l], color = "red", label = "Nominális")
+		plot(time_mem[1:l], q_mem[1:l], color = "green", linestyle = "--", label = "Megvalósult")
+		legend(loc = 1, borderaxespad = 0)
+		savefig("allplots_duffing.pdf")
 
-	# Plotting 
-	figure("Trajectory_tracking")
-	grid(true)
-	title("Pályakövetés az idő függvényében")
-	xlabel("Idő [s]")
-	ylabel("Pozíció [m]")
-	plot(time_mem[1:l], qN_mem[1:l], color = "red", label = "Nominális")
-	plot(time_mem[1:l], q_mem[1:l], color = "green", linestyle = "--", label = "Megvalósult")
-	legend(loc = 1, borderaxespad = 0)
-	savefig("allplots_duffing.pdf")
+		#velocity gyorsan kell
 
-	#velocity gyorsan kell
+		figure("Acceleration")
+		grid(true)
+		title("Accelerations vs Time")
+		xlabel("Time [s]")
+		ylabel("Acceleration [m/s²]")
+		# ylabel(L"Acceleration $\frac{m}{s^2}$")
+		plot(time_mem[1:l], qN_pp_mem[1:l], color = "#7684FF", label = "Névleges")
+		plot(time_mem[1:l], q_pp_mem[1:l], color = "#FFAA41", label = "Realizált", linestyle = "--")
+		plot(time_mem[1:l], qDes_pp_mem[1:l], color = "#2A40FF", label = "Desired", linestyle = "-.")
+		legend(loc = 1, borderaxespad = 0)
+		savefig("temp_duffing.pdf")
+		append_pdf!("allplots_duffing.pdf", "temp_duffing.pdf", cleanup = true)
 
-	figure("Acceleration")
-	grid(true)
-	title("Accelerations vs Time")
-	xlabel("Time [s]")
-	ylabel("Acceleration [m/s²]")
-	# ylabel(L"Acceleration $\frac{m}{s^2}$")
-	plot(time_mem[1:l], qN_pp_mem[1:l], color = "#7684FF", label = "Névleges")
-	plot(time_mem[1:l], q_pp_mem[1:l], color = "#FFAA41", label = "Realizált", linestyle = "--")
-	plot(time_mem[1:l], qDes_pp_mem[1:l], color = "#2A40FF", label = "Desired", linestyle = "-.")
-	legend(loc = 1, borderaxespad = 0)
-	savefig("temp_duffing.pdf")
-	append_pdf!("allplots_duffing.pdf", "temp_duffing.pdf", cleanup = true)
+		figure("Tracking_Error")
+		title("Követési hiba az idő függvényében")
+		grid(true)
+		xlabel("Idő [s]")
+		ylabel("Követési hiba [m]")
+		plot(time_mem[1:l], qN_mem[1:l] - q_mem[1:l], color = "red")
+		savefig("temp_duffing.pdf")
+		append_pdf!("allplots_duffing.pdf", "temp_duffing.pdf", cleanup = true)
 
-	figure("Tracking_Error")
-	title("Követési hiba az idő függvényében")
-	grid(true)
-	xlabel("Idő [s]")
-	ylabel("Követési hiba [m]")
-	plot(time_mem[1:l], qN_mem[1:l] - q_mem[1:l], color = "red")
-	savefig("temp_duffing.pdf")
-	append_pdf!("allplots_duffing.pdf", "temp_duffing.pdf", cleanup = true)
+		figure("Control_Signal")
+		grid(true)
+		title("Irányítójel az idő függvényében")
+		xlabel("Idő [s]")
+		ylabel("Irányítójel [N]")
+		plot(time_mem[1:l], u_mem[1:l], color = "red")
+		savefig("temp_duffing.pdf")
+		append_pdf!("allplots_duffing.pdf", "temp_duffing.pdf", cleanup = true)
 
-	figure("Control_Signal")
-	grid(true)
-	title("Irányítójel az idő függvényében")
-	xlabel("Idő [s]")
-	ylabel("Irányítójel [N]")
-	plot(time_mem[1:l], u_mem[1:l], color = "red")
-	savefig("temp_duffing.pdf")
-	append_pdf!("allplots_duffing.pdf", "temp_duffing.pdf", cleanup = true)
-
-	figure("Phase_Space")
-	grid(true)
-	title("Fázistér")
-	xlabel("Pozíció [m]")
-	ylabel("Sebesség [m/s]")
-	plot(qN_mem[1:l, :], qN_p_mem[1:l, :], color = "red", label = "Nominális")
-	plot(q_mem[1:l, :], q_p_mem[1:l, :], color = "green", linestyle = "--", label = "Megvalósult")
-	legend(loc = 1, borderaxespad = 0)
-	savefig("temp_duffing.pdf")
-	append_pdf!("allplots_duffing.pdf", "temp_duffing.pdf", cleanup = true)
-
+		figure("Phase_Space")
+		grid(true)
+		title("Fázistér")
+		xlabel("Pozíció [m]")
+		ylabel("Sebesség [m/s]")
+		plot(qN_mem[1:l, :], qN_p_mem[1:l, :], color = "red", label = "Nominális")
+		plot(q_mem[1:l, :], q_p_mem[1:l, :], color = "green", linestyle = "--", label = "Megvalósult")
+		legend(loc = 1, borderaxespad = 0)
+		savefig("temp_duffing.pdf")
+		append_pdf!("allplots_duffing.pdf", "temp_duffing.pdf", cleanup = true)
+	end
+	return qN_mem[max_index] - q_mem[max_index]
 	# date_string = Dates.format(now(), "mm-dd_HH-MM")
 	# fileName = "./Plots/Duffing/" * date_string * "_$idx.pdf"
 	# mv("allplots_duffing.pdf", fileName)
@@ -203,7 +210,7 @@ function singlerun(h_int, q, q_p, q_pp, qN_p, qN_pp, u, idx, lastPlot = true)
 	# end
 end
 
-function run_simulation(q0, q_p0, q_pp0, init_Amp, init_ω, t_range)
+function run_simulation(q_param, q_p_param, q_pp_param)
 	global time_mem = zeros(LONG)
 	global q_mem = zeros(LONG)
 	global q_p_mem = zeros(LONG)
@@ -213,28 +220,53 @@ function run_simulation(q0, q_p0, q_pp0, init_Amp, init_ω, t_range)
 	global qN_pp_mem = zeros(LONG)
 	global h_int = 0
 	global u_mem = zeros(LONG)
-	if (SingleRun == 1)
-		q_mem[1] = q0
-		q_p_mem[1] = q_p0
-		q_pp_mem[1] = q_pp0
-		singlerun(h_int, q_mem, q_p_mem, q_pp_mem, qN_p_mem, qN_pp_mem, u_mem, 1)
-	else
-		# Define initial conditions: (q0, q_p0)
-		condition_q = init_Amp .* sin.(init_ω .* t_range)
-		condition_qp = init_Amp * init_ω .* cos.(init_ω .* t_range)
-		for (idx, (q0, q_p0)) in enumerate(zip(condition_q, condition_qp))
-			# Initialize memory arrays again for each run
-			q_mem[1] = q0
-			q_p_mem[1] = q_p0
-			singlerun(h_int, q_mem, q_p_mem, q_pp_mem, qN_p_mem, qN_pp_mem, u_mem, idx, (idx == length(condition_q)))
+	position = zeros(441)
+	velocity = zeros(441)
+	error_max = zeros(441)
+	index = 1
+	for q0 in -1:0.1:1
+		for q_p0 in -1:0.1:1
+			position[index] = q0
+			velocity[index] = q_p0
+			error_max[index] = singlerun(q0, q_p0, false)
+			index += 1
 		end
 	end
-	# log()
-	if (Ploting == 1 && SingleRun == 1)
-		show()
-	end
-end
+	# for q0 in range(q_param[1], q_param[2], q_param[3])
+	# 	for q_p0 in range(q_p_param[1], q_p_param[2], q_p_param[3])
+	# 		max_index+=1
+	# 		max_values[max_index] = singlerun(q0, q_p0, false)
+	# 	end
+	# end
+	figure("Error_Max")
+	grid(true)
+	title("Hiba Tér")
+	xlabel("Pozíció [m]")
+	ylabel("Sebesség [m/s]")
+	plot(position[1:441], error_max[1:441], color = "red", label = "Hiba")
+	legend(loc = 1, borderaxespad = 0)
 
+	# if (SingleRun == 1)
+	# 	q_mem[1] = q0
+	# 	q_p_mem[1] = q_p0
+	# 	q_pp_mem[1] = q_pp0
+	# 	singlerun(q_mem, q_p_mem, q_pp_mem, qN_p_mem, qN_pp_mem, u_mem, 1)
+	# else
+	# 	# Define initial conditions: (q0, q_p0)
+	# 	condition_q = init_Amp .* sin.(init_ω .* t_range)
+	# 	condition_qp = init_Amp * init_ω .* cos.(init_ω .* t_range)
+	# 	for (idx, (q0, q_p0)) in enumerate(zip(condition_q, condition_qp))
+	# 		# Initialize memory arrays again for each run
+	# 		q_mem[1] = q0
+	# 		q_p_mem[1] = q_p0
+	# 		singlerun(h_int, q_mem, q_p_mem, q_pp_mem, qN_p_mem, qN_pp_mem, u_mem, idx, (idx == length(condition_q)))
+	# 	end
+	# end
+	# # log()
+	# if (Ploting == 1 && SingleRun == 1)
+	# 	show()
+	# end
+end
 
 ##############################
 # Define arrays for Plotting #
@@ -271,9 +303,10 @@ t_range = 0:δranget:t_max
 
 using PDFmerger
 using Dates
-run_simulation(q_mem[1], q_p_mem[1], q_pp_mem[1], init_Amp, init_ω, t_range)
+# run_simulation([-1, 1, 0.1], [-1, 1, 0.1], [0, 0, 0])
+run_simulation(1, 1, 1)
 # run_simulation(1, 5, 2, init_Amp, init_ω, t_range)
-
+# singlerun(q_mem[1], q_p_mem[1], q_pp_mem[1])
 
 
 show()
