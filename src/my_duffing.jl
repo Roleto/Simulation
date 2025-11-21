b3b581ef2b4c3beda16a0abd99fd10aabf4cc40b
Roland Gulya <mgulyaroland@gmail.com>
2025-11-21 19:48:56 +0100

    making it like in the master

--- src/my_duffing.jl
+++ src/my_duffing.jl
@@ -191,27 +191,23 @@ function singlerun(q, q_p, ploting = true)
 		savefig("temp_duffing.pdf")
 		append_pdf!("allplots_duffing.pdf", "temp_duffing.pdf", cleanup = true)
 	end
+
+	date_string = Dates.format(now(), "mm-dd_HH-MM")
+	random_hash = rand(1:100)
+	fileName = "../Data/Duffing/" * date_string * "_$random_hash" * ".pdf"
+	mv("allplots_duffing.pdf", fileName)
 	return qN_mem[max_index] - q_mem[max_index]
-	# date_string = Dates.format(now(), "mm-dd_HH-MM")
-	# fileName = "./Plots/Duffing/" * date_string * "_$idx.pdf"
-	# mv("allplots_duffing.pdf", fileName)
-	# if (lastPlot && Ploting == 0)
-	# 	close("all")
-	# 	figure("trajectoria")
-	# 	grid(true)
-	# 	title("Trajectory  Run 1 Robust = $Robust \n (q₀=$(q[1]), q̇₀=$(q_p[1]))")
-	# 	xlabel("Time [s]")
-	# 	ylabel("position")
-	# 	plot(time_mem[1:l], qN_mem[1:l], color = "red", label = "Névleges")
-	# 	plot(time_mem[1:l], q_mem[1:l], color = "green", linestyle = "--", label = "Realizált")
-	# 	legend(loc = 1, borderaxespad = 0)
-	# 	savefig("./Plots/Duffing/TEMP/traj_run_$idx.png")
-	# end
 end
 
-function run_simulation(q_param, q_p_param, q_pp_param)
-	global time_mem = zeros(LONG)
-	global q_mem = zeros(LONG)
+"""
+	init_duffing(;plot=true)
+
+Allocate/initialize globals for Duffing simulation. Returns initial (q0, q_p0).
+Set `Ploting` externally if desired. Use returned values to call `duffing_single_run`.
+"""
+function init_duffing(; plot = true)
+	global time_mem = zeros(LONG) #t
+	global q_mem = zeros(LONG) # realized trajectory
 	global q_p_mem = zeros(LONG)
 	global q_pp_mem = zeros(LONG)
 	global qN_mem = zeros(LONG) # nominal trajectory
@@ -221,54 +217,58 @@ function run_simulation(q_param, q_p_param, q_pp_param)
 	global qDes_pp_mem = zeros(LONG)
 	global qDef_pp_mem = zeros(LONG)
 	global h_int = 0
-	global u_mem = zeros(LONG)
-	position = zeros(441)
-	velocity = zeros(441)
-	error_max = zeros(441)
-	index = 1
-	for q0 in -1:0.1:1
-		for q_p0 in -1:0.1:1
-			position[index] = q0
-			velocity[index] = q_p0
-			error_max[index] = singlerun(q0, q_p0, false)
-			index += 1
-		end
-	end
-	# for q0 in range(q_param[1], q_param[2], q_param[3])
-	# 	for q_p0 in range(q_p_param[1], q_p_param[2], q_p_param[3])
-	# 		max_index+=1
-	# 		max_values[max_index] = singlerun(q0, q_p0, false)
-	# 	end
-	# end
-	figure("Error_Max")
-	grid(true)
-	title("Hiba Tér")
-	xlabel("Pozíció [m]")
-	ylabel("Sebesség [m/s]")
-	plot(position[1:441], error_max[1:441], color = "red", label = "Hiba")
-	legend(loc = 1, borderaxespad = 0)
-
-	# if (SingleRun == 1)
-	# 	q_mem[1] = q0
-	# 	q_p_mem[1] = q_p0
-	# 	q_pp_mem[1] = q_pp0
-	# 	singlerun(q_mem, q_p_mem, q_pp_mem, qN_p_mem, qN_pp_mem, u_mem, 1)
-	# else
-	# 	# Define initial conditions: (q0, q_p0)
-	# 	condition_q = init_Amp .* sin.(init_ω .* t_range)
-	# 	condition_qp = init_Amp * init_ω .* cos.(init_ω .* t_range)
-	# 	for (idx, (q0, q_p0)) in enumerate(zip(condition_q, condition_qp))
-	# 		# Initialize memory arrays again for each run
-	# 		q_mem[1] = q0
-	# 		q_p_mem[1] = q_p0
-	# 		singlerun(h_int, q_mem, q_p_mem, q_pp_mem, qN_p_mem, qN_pp_mem, u_mem, idx, (idx == length(condition_q)))
-	# 	end
-	# end
-	# # log()
-	# if (Ploting == 1 && SingleRun == 1)
-	# 	show()
-	# end
+	# initial state from nominal trajectory at first time step
+	q_mem[1] = Amp * sin(ω * δt)
+	q_p_mem[1] = Amp * ω * cos(ω * δt)
+	q_pp_mem[1] = -Amp * ω^2 * sin(ω * δt)
+	return q_mem[1], q_p_mem[1]
 end
+
+# Convenience wrapper to call original singlerun after init
+function duffing_single_run(; plot = true)
+	q0, q_p0 = init_duffing(; plot = plot)
+	max_error = singlerun(q0, q_p0, plot)
+	if plot
+		show()
+	end
+	return max_error
+end
+
+# ---------------------------------------------------------------
+# Archived original multi-run grid search (run_simulation)
+# Keeping for future reference; can be re-enabled or refactored.
+#
+# function run_simulation(q_param, q_p_param, q_pp_param)
+#   global time_mem = zeros(LONG)
+#   global q_mem = zeros(LONG)
+#   global q_p_mem = zeros(LONG)
+#   global q_pp_mem = zeros(LONG)
+#   global qN_mem = zeros(LONG)
+#   global qN_p_mem = zeros(LONG)
+#   global qN_pp_mem = zeros(LONG)
+#   global h_int = 0
+#   global u_mem = zeros(LONG)
+#   position = zeros(441)
+#   velocity = zeros(441)
+#   error_max = zeros(441)
+#   index = 1
+#   for q0 in -1:0.1:1
+#       for q_p0 in -1:0.1:1
+#           position[index] = q0
+#           velocity[index] = q_p0
+#           error_max[index] = singlerun(q0, q_p0, false)
+#           index += 1
+#       end
+#   end
+#   figure("Error_Max")
+#   grid(true)
+#   title("Hiba Tér")
+#   xlabel("Pozíció [m]")
+#   ylabel("Sebesség [m/s]")
+#   plot(position[1:441], error_max[1:441], color = "red", label = "Hiba")
+#   legend(loc = 1, borderaxespad = 0)
+# end
+# ---------------------------------------------------------------
 
 ##############################
 # Define arrays for Plotting #
@@ -284,13 +284,3 @@ init_Amp = 2
 # időlépések
 δranget = 1
 t_range = 0:δranget:t_max
-
-using PDFmerger
-using Dates
-# run_simulation([-1, 1, 0.1], [-1, 1, 0.1], [0, 0, 0])
-run_simulation(1, 1, 1)
-# run_simulation(1, 5, 2, init_Amp, init_ω, t_range)
-# singlerun(q_mem[1], q_p_mem[1], q_pp_mem[1])
-
-
-show()
